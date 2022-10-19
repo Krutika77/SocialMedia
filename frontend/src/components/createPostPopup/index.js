c639a6c38c4dc27edde27a22748f6e7accb6b3ab
Krutika77 <40793266+Krutika77@users.noreply.github.com>
2022-10-18 23:32:41 -0400

    Create post

--- frontend/src/components/createPostPopup/index.js
+++ frontend/src/components/createPostPopup/index.js
@@ -3,11 +3,12 @@ import "./style.css";
 import Picker from "emoji-picker-react";
 import EmojiPickerBackgrounds from "./EmojiPickerBackgrounds";
 import AddToYourPost from "./AddToYourPost";
+import ImagePreview from "./ImagePreview";
 
 export default function CreatePostPopup({ user }) {
   const [text, setText] = useState("");
-  const [showPrev, setShowPrev] = useState(false);
-  const textRef = useRef(null);
+  const [showPrev, setShowPrev] = useState(true);
+  const [images, setImages] = useState([]);
 
   return (
     <div className="blur">
@@ -31,26 +32,26 @@ export default function CreatePostPopup({ user }) {
             </div>
           </div>
         </div>
-        {!showPrev && (
+        {!showPrev ? (
           <>
-            <div className="flex_center">
-              <textarea
-                ref={textRef}
-                maxLength="100"
-                value={text}
-                placeholder={`Share your thoughts with us, ${user.first_name}`}
-                className="post_input"
-                onChange={(e) => setText(e.target.value)}
-              ></textarea>
-            </div>
             <EmojiPickerBackgrounds
               text={text}
-              textRef={textRef}
+              user={user}
               setText={setText}
+              // showPrev={showPrev}
             />
           </>
+        ) : (
+          <ImagePreview
+            text={text}
+            user={user}
+            setText={setText}
+            setShowPrev={setShowPrev}
+            images={images}
+            setImages={setImages}
+          />
         )}
-        <AddToYourPost />
+        <AddToYourPost setShowPrev={setShowPrev} />
         <button className="post_submit">Post</button>
       </div>
     </div>
