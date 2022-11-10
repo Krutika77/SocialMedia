16f740f6498301415228b9657e509d246ddf2881
Krutika77 <40793266+Krutika77@users.noreply.github.com>
2022-11-09 22:23:37 -0500

    delete post

--- frontend/src/components/post/index.js
+++ frontend/src/components/post/index.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import Moment from "react-moment";
 import { Dots, Public } from "../../svg";
 import ReactsPopup from "./ReactsPopup";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import CreateComment from "./CreateComment";
 import PostMenu from "./PostMenu";
 import { useEffect } from "react";
@@ -59,8 +59,13 @@ export default function Post({ post, user, profile }) {
   const showMore = () => {
     setCount((prev) => prev + 3);
   };
+  const postRef = useRef(null);
   return (
-    <div className="post" style={{ width: `${profile && "100%"}` }}>
+    <div
+      className="post"
+      style={{ width: `${profile && "100%"}` }}
+      ref={postRef}
+    >
       <div className="post_header">
         <Link
           to={`/profile/${post.user.username}`}
@@ -164,9 +169,13 @@ export default function Post({ post, user, profile }) {
                   return b.count - a.count;
                 })
                 .map(
-                  (react) =>
+                  (react, i) =>
                     react.count > 0 && (
-                      <img src={`../../../reacts/${react.react}.svg`} alt="" />
+                      <img
+                        src={`../../../reacts/${react.react}.svg`}
+                        alt=""
+                        key={i}
+                      />
                     )
                 )}
           </div>
@@ -270,6 +279,7 @@ export default function Post({ post, user, profile }) {
           checkSaved={checkSaved}
           setCheckSaved={setCheckSaved}
           images={post.images}
+          postRef={postRef}
         />
       )}
     </div>
--- frontend/src/functions/reducers.js
+++ frontend/src/functions/reducers.js
@@ -34,7 +34,7 @@ export function profileReducer(state, action) {
     case "PROFILE_POST":
       return {
         loading: false,
-        profile: { ...state, posts: action.payload },
+        profile: { ...state.profile, posts: action.payload },
         error: "",
       };
     case "PROFILE_ERROR":
